e76a2156dd41216d4fadb177d176de6a36439a44
Ayobami Olarewaju-Alo <ayobami.dev@gmail.com>
2018-05-25 18:32:45 +0100

    Pass application port in as env variable, or default to 80

--- index.js
+++ index.js
@@ -27,6 +27,27 @@ app.use(express.static(path.join(__dirname, 'dist')));
 // Routes
 app.use('/', IndexController)
 
-app.listen(3000, () => console.log('Transfer Estimates listening on port 3000!'));
+var port = normalizePort(process.env.PORT || '80');
+app.listen(port, () => console.log(`Transfer Estimates listening on port ${port}!`));
 
 // TODO: Catch errors (404, 500). 'http-errors'
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+	var port = parseInt(val, 10);
+
+	if (isNaN(port)) {
+		// named pipe
+		return val;
+	}
+
+	if (port >= 0) {
+		// port number
+		return port;
+	}
+
+	return false;
+}
